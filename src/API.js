6ba074a0a240d4677a60b082a00fbf13deebd1fd
superstevy <74840377+superstevy@users.noreply.github.com>
2022-01-21 16:53:33 -0500

    Update API.js

--- src/API.js
+++ src/API.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+// using axios to fetch datas from https://fakestoreapi.com/
+
 const baseURL = "https://fakestoreapi.com/";
 const api = axios.create({
   baseURL: baseURL,
