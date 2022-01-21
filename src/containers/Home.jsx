d741f2554e62a4c2a8852be355388698fee0807b
superstevy <74840377+superstevy@users.noreply.github.com>
2022-01-21 16:54:40 -0500

    Update Home.jsx

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -7,17 +7,24 @@ import API from "../API";
 const api = new API();
 
 const Home = () => {
+  // The array products will be used to store the datas fetched from the api
   const [products, setProducts] = useState([]);
+
+  // selectedProductId will take the value of the id of a selected product
   const [selectedProductId, setSelectedProductId] = useState(null);
+
   const cardClick = (productId) => {
     console.log(productId);
     selectedProductId(productId);
   };
+
+  // fetching product datas with a limit of 5
   const limit = 5;
   useEffect(() => {
     api
       .getProducts({ limit })
       .then((response) => {
+        // storing datas fetched in the products array
         setProducts(response.data);
         setSelectedProductId(response.data[0].id);
       })
@@ -30,19 +37,30 @@ const Home = () => {
       <Heading />
       <section className="main">
         <div className="card-list">
+          {/* Checking if any data exist in the products array then mapping them out on the screen */}
           {products.length > 0 && (
             <>
               {products.map((product) => (
-                <Card key={product.id} product={product} selectedProductId={selectedProductId} setSelectedProductId={setSelectedProductId} />
+                <Card
+                  key={product.id}
+                  product={product}
+                  selectedProductId={selectedProductId}
+                  setSelectedProductId={setSelectedProductId}
+                />
               ))}
             </>
           )}
         </div>
         <div className="product-details">
+          {/* For each listed item the description and price will also be listed */}
           {products.length > 0 && (
             <>
               {products.map((product) => (
-                <ProductDetail key={product.id} product={product} selectedProductId={selectedProductId} />
+                <ProductDetail
+                  key={product.id}
+                  product={product}
+                  selectedProductId={selectedProductId}
+                />
               ))}
             </>
           )}
